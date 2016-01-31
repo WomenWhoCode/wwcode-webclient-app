61011a6c9e6248e03c6adce5a4c613e0fc88f2c4
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2016-01-30 23:49:11 -0800

    took care of events routes with resources :events

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  get '/', to: "events#index"
+  resources :events
 end
