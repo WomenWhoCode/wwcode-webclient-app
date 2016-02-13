272cdc926613039e81e00b5cf4d9d6c49b56f457
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2016-02-13 13:39:46 -0800

    added highlighting for hovering and selecting events

--- app/assets/javascripts/events_ctrl.js
+++ app/assets/javascripts/events_ctrl.js
@@ -6,14 +6,46 @@
     $scope.setup = function() {
       $scope.events=convertEventsDates($scope.events);
       $scope.events.selected_id=null;
+      $scope.setupColors();
     //   $http.get("/api/v1/people.json").then(function(response) {
     //     $scope.people = response.data;
     //     console.log(response);
     //   });
     }
-    $scope.select_event = function(id) {
-      $scope.events.selected_id=id;
+
+    $scope.setupColors = function(){ $scope.events.defaultBgColorHead="#00B6AA";
+      $scope.events.defaultBgColorBody="#FAFAFA";
+      for (var i=0;i<$scope.events.length;i++){   
+        $scope.events[i].bgColorHead=$scope.events.defaultBgColorHead;
+        $scope.events[i].bgColorBody=$scope.events.defaultBgColorBody;
+      }
     }
+
+    $scope.select_event = function(event) {
+      $scope.events.selected_id=event.id;
+      $scope.setupColors();//reset the color settings so that any other events that were previously clicked are no longer highlighted
+      $scope.mouseoverEvent(event);
+    }
+
+    $scope.highlightSelectedEvent =function(event){
+      event.bgColorHead="#009688";
+    }
+
+    $scope.mouseoverEvent = function(event){
+      event.bgColorHead="#faa73f";
+      event.bgColorBody="#EEEEEE";
+    }
+
+    $scope.mouseleaveEvent = function(event){
+      if (event.id == $scope.events.selected_id){
+        $scope.        highlightSelectedEvent(event);
+      }
+      else{
+      event.bgColorHead=$scope.events.defaultBgColorHead;
+      event.bgColorBody=$scope.events.defaultBgColorBody;
+      }
+    }
+
     $scope.events = [
         { 
           id: 1,
