ced05e42a79314a9adf6ca400e0c4ad3ac6047f5
Mark Richardson <markeban@yahoo.com>
2016-02-29 15:12:09 -0800

    made RESTful, fixed Unirest call

--- app/controllers/user_accounts_controller.rb
+++ app/controllers/user_accounts_controller.rb
@@ -10,29 +10,30 @@ class UserAccountsController < ApplicationController
   end
   
   def create
-    response = Unirest.post "http://staging-wwcode-api.herokuapp.com/auth/sign_in",
+    response = Unirest.post "http://localhost:3000/auth",
       headers: {"Accept" => "application/json" },
       parameters: {
         email: params[:email],
-        password: params[:password]
-      } {|response|
-        response.headers
-      }  
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+      } 
+    puts "coolness"
+    p response.body
     redirect_to "/profiles"  
   end
 
-  def new
-    response = Unirest.post "http://staging-wwcode-api.herokuapp.com/auth",
-      headers: {"Accept" => "application/json" },
-      parameters: {
-        personalization_details: session[:form_data],
-        username: params[:username],
-        email: params[:email],
-        password: params[:password],
-        password_confirmation: params[:password_confirmation],
-        access_code: params[:access_code]
-      }
-    redirect_to "/profiles"   
-  end
+  # def new
+  #   response = Unirest.post "http://staging-wwcode-api.herokuapp.com/auth",
+  #     headers: {"Accept" => "application/json" },
+  #     parameters: {
+  #       personalization_details: session[:form_data],
+  #       username: params[:username],
+  #       email: params[:email],
+  #       password: params[:password],
+  #       password_confirmation: params[:password_confirmation],
+  #       access_code: params[:access_code]
+  #     }
+  #   redirect_to "/profiles"   
+  # end
 
 end
