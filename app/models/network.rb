e46bb420a2b43bb2540d2e3dcc1adc94fc91ea7c
Tiffany Price <tiffany.hood@stanfordalumni.org>
2016-02-28 16:45:34 -0800

    Building PORO for network model

---
+++ app/models/network.rb
@@ -0,0 +1,35 @@
+class Network 
+
+  attr_accessor :id,
+    :objectId,
+    :awesome_count,
+    :image_url,
+    :latitude,
+    :longitude,
+    :meetup_group_id,
+    :time_zone,
+    :title
+
+  def initialize(hash)
+    @id = hash["id"]
+    @awesome_count = hash["awesome_count"]
+    @image_url = hash["image_url"]
+    @latitude = hash["latitude"]
+    @longitude = hash["longitude"]
+    @meetup_group_id = hash["meetup_group_id"]
+    @time_zone = hash["time_zone"]
+    @title = hash["title"]
+  end
+
+  def self.all
+    networks = []
+    networks_hashes = Unirest.get("http://staging-wwcode-api.herokuapp.com/api/v1/networks.json",
+        headers: {"Accept" => "application/json"},
+        parameters: attributes).body
+    networks_hashes.each do |networks_hash|
+      networks << Network.new(network_hash)
+    end
+    return networks
+  end
+
+end
