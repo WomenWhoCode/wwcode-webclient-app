234262fa15265c1c31b2270b9980fa65d63d379f
Luke Evans <luevans248@gmail.com>
2016-03-03 15:39:30 -0800

    removed some front end validations, changed how buttons behave when hovered over for onboarding, fixed checkboxes

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,11 +4,11 @@ class Session
     new_session_hash = Unirest.post("#{ENV['API_DOMAIN']}/auth/sign_in",
               headers: {"Accept" => "application/json"},
               parameters: attributes)
+    
     return new_session_hash
   end
 
   def self.destroy(authentication_headers)
-    puts authentication_headers
     Unirest.delete("#{ENV['API_DOMAIN']}/auth/sign_out",
               headers: authentication_headers)
   end
